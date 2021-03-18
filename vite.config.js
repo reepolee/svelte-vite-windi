039e50f2167ea500e395dfb376a0f0fb95657041
Ales Vaupotic <ales@vaupotic.eu>
2021-03-18 22:06:31 +0100

    vite build mode insted of NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig} from "vite";
 import svelte from "@svitejs/vite-plugin-svelte";
 import windi from "svelte-windicss-preprocess";
-const production = process.env.NODE_ENV === "production";
 
-export default defineConfig({
-  plugins: [
-    svelte({
-      preprocess: [
-        windi.preprocess({
-          config: "windi.config.js",
-          compile: false,
-          mode: production ?  "prod" : "dev" 
-          // prefix: "windi-",
-          // verbose: true,
-          // silent: false,
-          // debug: true,
-        }),
-      ],
-      emitCss: true,
-      hot: !production,
-    }),
-  ],
+export default defineConfig(({ command, mode }) => {
+  const production = mode === "production";
+  return {
+    plugins: [
+      svelte({
+        preprocess: [
+          windi.preprocess({
+            config: "windi.config.js",
+            compile: false,
+            mode: production ? "prod" : "dev",
+            // prefix: "windi-",
+            // verbose: true,
+            // silent: false,
+            // debug: true,
+          }),
+        ],
+        emitCss: true,
+        hot: !production,
+      }),
+    ],
+  }
+
 });
